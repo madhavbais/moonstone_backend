0b5735f4504ac4cf9b224943fc4b25cc4ffba793
itish <itishjain7@gmail.com>
2024-03-08 17:13:53 +0530

    added changes in event route

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -7,7 +7,7 @@ Router.get('/alleventData',GetAllevents);
 Router.get('/geteventBycategory',GetEventDataByCategory);
 Router.get('/:eventid',geteventById)
 Router.post('/addData',authMiddleware,isAdmin,addEvent);
-Router.post('/addData',authMiddleware,isAdmin,checkevent);
+Router.post('/checkevent',authMiddleware,isAdmin,checkevent);
 Router.put('/updateEvent/:eventid',authMiddleware,isAdmin,updateEvent);
 Router.delete('/:eventid',authMiddleware,isAdmin,deleteEvent)
 module.exports = Router;
